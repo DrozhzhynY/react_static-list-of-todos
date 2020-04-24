17d6fcf9bd8474f9d1d01892cd5e381ff4ee6e35
Yevhenii <DrozhzhynY>
2020-04-24 18:01:54 +0300

    Updates

---
+++ precache-manifest.a8bfd2eab0e817176e75087e66d38a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0d95ec8c22481cc89b848cf3204fb9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2185a1232d43442d20e",
+    "url": "./static/css/main.facbaff9.chunk.css"
+  },
+  {
+    "revision": "cdbfa4b0ed9a17660ba3",
+    "url": "./static/js/2.b0f90b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b0f90b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2185a1232d43442d20e",
+    "url": "./static/js/main.c04e9e96.chunk.js"
+  },
+  {
+    "revision": "dc13ecc3e0fecfe0c39b",
+    "url": "./static/js/runtime-main.33c19533.js"
+  }
+]);
